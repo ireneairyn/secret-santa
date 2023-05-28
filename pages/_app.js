d7d70940bdf4f33f4bc78cfc19953a52654c19ab
Irene Aunon <irene@ireneaunion.com>
2023-05-28 21:20:19 +0200

    add background image

--- pages/_app.js
+++ pages/_app.js
@@ -5,40 +5,36 @@ import Link from "next/link";
 import Image from "next/image";
 import "@/styles/globals.css";
 
-
 export default function App({ Component, pageProps }) {
   return (
     <>
-          <div className="background"></div>
-          <div className="topLinks">
-          <a
-            href="https://github.com/ireneairyn/secret-santa"
-            className="logo-link"
-            target="_blank"
-          >
-            <Image
-              src="/github-mark-white.png"
-              alt="github logo"
-              width={35}
-              height={35}
-            />
-          </a>{" "}
-          <a
-            href="https://www.amazon.co.uk/"
-            className="emoji-link"
-            target="_blank"
-          >
-            <Image
-              src="/holly.png"
-              alt="holly logo"
-              width={40}
-              height={40}
-            />
-          </a>
-        </div>
+      <div className="topLinks">
+        <a
+          href="https://github.com/ireneairyn/secret-santa"
+          className="logo-link"
+          target="_blank"
+        >
+          <Image
+            src="/github-mark-white.png"
+            alt="github logo"
+            width={35}
+            height={35}
+          />
+        </a>{" "}
+        <a
+          href="https://www.amazon.co.uk/"
+          className="emoji-link"
+          target="_blank"
+        >
+          <Image src="/holly.png" alt="holly logo" width={40} height={40} />
+        </a>
+      </div>
       <Head>
         <title>Secret Santa</title>
-        <meta name="description" content="Plan and organize your Secret Santa party!" />
+        <meta
+          name="description"
+          content="Plan and organize your Secret Santa party!"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -49,8 +45,8 @@ export default function App({ Component, pageProps }) {
           </Link>
         </header>
         <Component {...pageProps} />
-        <footer>
-          <img className={styles.present} src="/present.png" />
+        <footer className={styles.footer}>
+          <div className={styles.silhouette} />
         </footer>
       </div>
     </>
