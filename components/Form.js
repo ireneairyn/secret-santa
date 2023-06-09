7b21d513ccc06e95c92e5c702d78edf371c0a7cf
Irene Aunon <irene@ireneaunion.com>
2023-06-09 14:01:27 +0200

    add delete participants button and fix style of create even page

--- components/Form.js
+++ components/Form.js
@@ -7,62 +7,68 @@ function Form() {
   const [time, setTime] = useState("");
   const [location, setLocation] = useState("");
   const [budget, setBudget] = useState("");
+  const [message, setMessage] = useState("");
   const [users, setUsers] = useState([{ name: "", email: "" }]);
 
   const addParticipant = React.useCallback(() => {
     setUsers([...users, { name: "", email: "" }]);
   }, [users]);
 
+  const deleteParticipant = (index) => {
+    const updatedUsers = [...users];
+    updatedUsers.splice(index, 1);
+    setUsers(updatedUsers);
+  };
+
   const send = React.useCallback(() => {
-    console.log("Send", { users });
-  }, [users]);
+    console.log("Send", { users, message });
+  }, [users, message]);
 
   return (
     <div className="form">
-      <div className="line">
-        <label htmlFor="event">🎉 Event</label>
-        <input
-          id="event"
-          type="text"
-          value={event}
-          onChange={(e) => setEvent(e.target.value)}
-        />
-        <div className="dateTime">
-          <div>
-            <label htmlFor="date">📅 Date</label>
-            <input
-              id="date"
-              type="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-            />
-          </div>
-          <div>
-            {" "}
-            <label htmlFor="time">🕒️ Time</label>
-            <input
-              id="time"
-              type="time"
-              value={time}
-              onChange={(e) => setTime(e.target.value)}
-            />
-          </div>
+      <label htmlFor="event">🎉 Event</label>
+      <input
+        id="event"
+        type="text"
+        value={event}
+        onChange={(e) => setEvent(e.target.value)}
+      />
+      <div className="dateTime">
+        <div>
+          <label htmlFor="date">📅 Date</label>
+          <input
+            id="date"
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
         </div>
-        <label htmlFor="location">📍 Location</label>
-        <input
-          id="budget"
-          type="text"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <label htmlFor="budget">💰️ Budget</label>
-        <input
-          id="budget"
-          type="text"
-          value={budget}
-          onChange={(e) => setBudget(e.target.value)}
-        />
+        <div>
+          {" "}
+          <label htmlFor="time">🕒️ Time</label>
+          <input
+            id="time"
+            type="time"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+        </div>
       </div>
+      <label htmlFor="location">📍 Location</label>
+      <input
+        id="budget"
+        type="text"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <label htmlFor="budget">💰️ Budget</label>
+      <input
+        id="budget"
+        type="text"
+        value={budget}
+        onChange={(e) => setBudget(e.target.value)}
+      />
+      <div className="line"></div>
       {users.map((user, index) => {
         return (
           <div key={index} className="participant">
@@ -81,7 +87,7 @@ function Form() {
               />
             </div>
             <div>
-              <label htmlFor={`email-${index}`}>📩 Email</label>
+              <label htmlFor={`email-${index}`}>✉️ Email</label>
               <input
                 id={`email-${index}`}
                 className="email"
@@ -94,6 +100,12 @@ function Form() {
                 }}
               />
             </div>
+            <button
+              className="deleteButton"
+              onClick={() => deleteParticipant(index)}
+            >
+              🗑️
+            </button>
           </div>
         );
       })}
@@ -101,6 +113,18 @@ function Form() {
         <button type="submit" onClick={addParticipant}>
           Add participant
         </button>
+      </div>
+      <div className="line"> </div>
+      <div>
+        <label htmlFor="message">📜 Message to Guests</label>
+        <input
+          id="message"
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </div>
+      <div className="buttonContainer">
         <button type="submit" onClick={send}>
           Send emails!
         </button>
